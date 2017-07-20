efe83648bed0ebd21dad2239894dfaeb9268c96c
dfingolo <zinho_dias@hotmail.com>
2017-07-20 15:19:06 -0300

    create list and form users

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,18 @@ const User = require(process.env.PWD + '/models/user')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   User.findAll().then(function(users) {
-    res.send(users)
+    res.render('users/index', {users})
   })
+})
+
+router.get('/new', function(req, res, next) {
+  let user = {
+    first_name: null,
+    last_name: null,
+    email: null,
+    birth_date: null
+  }
+  res.render('users/new', {user})
 })
 
 module.exports = router
